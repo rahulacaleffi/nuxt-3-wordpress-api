cf209adbb38077d960af0446fedc336c7c1120a8
Rahula Palu <rahula_dd@hotmail.com>
2024-03-27 21:08:01 -0300

    Added devtools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/content', '@nuxtjs/tailwindcss'],
+  modules: ["@nuxt/content", "@nuxtjs/tailwindcss", "@nuxt/devtools"],
   vue: {
     compilerOptions: {
-      isCustomElement: (tag) => ['UseFetchDemo'].includes(tag),
+      isCustomElement: (tag) => ["UseFetchDemo"].includes(tag),
+    },
+  },
+  runtimeConfig: {
+    public: {
+      graphqlUrl: "https://26.208.233.92/",
     },
   },
   components: {
     global: true,
-    dirs: ['~/components'],
+    dirs: ["~/components"],
   },
   content: {
     // https://content.nuxtjs.org/api/configuration
     highlight: {
-      preload: ['javascript', 'vue', 'html'],
-      theme: 'monokai',
+      preload: ["javascript", "vue", "html"],
+      theme: "monokai",
     },
   },
 });
